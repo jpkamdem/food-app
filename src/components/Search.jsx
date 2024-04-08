9e6114cb3340adcbb253d80b19e4acc974432704
jpkamdem <jpkamdem@proton.me>
2024-04-08 18:00:16 +0200

    styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import styles from './search.module.css'
 
 const apiUrl = "https://api.spoonacular.com/recipes/complexSearch";
 const apiKey = "2841e956276c4adf8f1aed1b47212b49";
@@ -16,8 +17,8 @@ export default function Search({ foodData, setFoodData }) {
   }, [query]);
 
   return (
-    <div>
-      <input
+    <div className={styles.searchContainer}>
+      <input className={styles.inputField}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
         value={query}
